407564d190b4704ca93b9aabcec727f045e9f8b8
João Junior <joao.jr13@icloud.com>
2022-10-24 23:47:39 -0300

    feat: criação do componente de Sidebar

---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,31 @@
+import styles from './Sidebar.module.css';
+import { PencilSimpleLine } from 'phosphor-react'
+
+export function Sidebar() {
+  return (
+    <aside className={styles.sidebar}>
+      <img
+        className={styles.cover}
+        src="https://images.unsplash.com/photo-1619410283995-43d9134e7656?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=50"
+      />
+
+      <div className={styles.profile}>
+        <img
+          className={styles.avatar}
+          src="https://github.com/joaojr13.png"
+        />
+        <strong>João Junior</strong>
+        <span>Web Developer</span>
+
+      </div>
+
+      <footer>
+        <a href="#">
+          <PencilSimpleLine size={20}/>
+          Editar seu perfil
+        </a>
+      </footer>
+
+    </aside>
+  )
+}
