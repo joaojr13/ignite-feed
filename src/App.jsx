bee40dcbdc81a0e626e986fc4a1b900a09a58fe0
João Junior <joao.jr13@icloud.com>
2022-10-27 00:44:13 -0300

    feat: implementação da utilização de iterações no JSX e propriedades para os componentes

--- src/App.jsx
+++ src/App.jsx
@@ -4,17 +4,64 @@ import { Post } from './components/Post';
 
 import './global.css';
 import styles from './App.module.css';
+// author : { avatar_url: "", name: "", role: ""}
+// publishedAt: Date
+// content: String
+
+const posts = [
+  {
+    id: 1,
+    author: {
+      avatarUrl: 'https://github.com/joaojr13.png',
+      name: 'João Junior',
+      role: 'Full Stack @ Kria Tecnologia'
+    },
+    content: [
+      { type: 'paragraph', content: 'Fala galeraa 👋' },
+      { type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀' },
+      { type: 'link', content: '👉 jane.design/doctorcare' },
+      { type: 'hash', content: '#novoprojeto' },
+      { type: 'hash', content: '#nlw' },
+      { type: 'hash', content: '#rocketseat' }
+    ],
+    publishedAt: new Date('2022-10-27 00:01:20')
+  },
+  {
+    id: 2,
+    author: {
+      avatarUrl: 'https://github.com/maykbrito.png',
+      name: 'Mayk Brito',
+      role: 'Educator @ Rocketseat'
+    },
+    content: [
+      { type: 'paragraph', content: 'Fala galeraa 👋' },
+      { type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀' },
+      { type: 'link', content: '👉 jane.design/doctorcare' },
+      { type: 'hash', content: '#novoprojeto' },
+      { type: 'hash', content: '#nlw' },
+      { type: 'hash', content: '#rocketseat' }
+    ],
+    publishedAt: new Date('2022-10-26 00:21:20')
+  },
+];
 
 function App() {
   return (
-    <div> 
-      <Header/>
-      
+    <div>
+      <Header />
+
       <div className={styles.wrapper}>
-        <Sidebar/>
+        <Sidebar />
         <main>
-          <Post/>
-          <Post/>
+          {posts.map(post => {
+            return (
+              <Post
+                author={post.author}
+                content={post.content}
+                publishedAt={post.publishedAt}
+              />
+            )
+          })}
         </main>
       </div>
     </div>
