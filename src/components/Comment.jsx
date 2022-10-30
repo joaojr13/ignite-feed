5ea0642276c6737aaeaa8d161476899a532a7e79
João Junior <joao.jr13@icloud.com>
2022-10-30 18:22:17 -0300

    feat: adicionando funcionalidades nos componentes

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -2,7 +2,12 @@ import { ThumbsUp, Trash } from 'phosphor-react';
 import { Avatar } from './Avatar';
 import styles from './Comment.module.css';
 
-export function Comment() {
+export function Comment({ content, onDeleteComment }) {
+
+  function handleDeleteComment() {
+    onDeleteComment(content)
+  }
+
   return (
     <div className={styles.comment}>
 
@@ -21,12 +26,15 @@ export function Comment() {
               <time title="11 de Maio às 08h00" dateTime="2022-05-11 08:00:00">Cerca de 1h atrás</time>
             </div>
 
-            <button title='Deletar comentário'>
+            <button
+              onClick={handleDeleteComment}
+              title='Deletar comentário'
+            >
               <Trash size={24} />
             </button>
           </header>
 
-          <p>Muito bom Devon, parabéns!! 👏👏</p>
+          <p>{content}</p>
         </div>
 
         <footer>
--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -8,7 +8,9 @@ import styles from './Post.module.css';
 
 export function Post({ author, publishedAt, content }) {
 
-  const [comments, setComments] = useState([1, 2])
+  const [comments, setComments] = useState(['Post muito bacana, hein?!']);
+
+  const [newCommentText, setNewCommentText] = useState('');
 
   const publishedDateFormatted = format(publishedAt, "dd 'de' LLLL 'às' HH:mm'h'", {
     locale: ptBR
@@ -21,8 +23,28 @@ export function Post({ author, publishedAt, content }) {
 
   function handleCreateNewComment() {
     event.preventDefault()
-    setComments([...comments, comments.length++])
+    setComments([...comments, newCommentText]);
+    setNewCommentText('');
+  };
+
+  function handleNewCommentChange() {
+    event.target.setCustomValidity('');
+    setNewCommentText(event.target.value);
   }
+
+  function handleNewCommentInvalid() {
+    event.target.setCustomValidity('Este campo é obrigatório!!!');
+  }
+
+  function deleteComment(commentToDelete) {
+    const commentsWithoutDeletedOne = comments.filter(comment => {
+      return comment != commentToDelete
+    });
+
+    setComments(commentsWithoutDeletedOne);
+  }
+
+  const isNewCommentEmpty = newCommentText.length === 0;
 
   return (
     <article className={styles.post}>
@@ -49,11 +71,11 @@ export function Post({ author, publishedAt, content }) {
       <div className={styles.content}>
         {content.map(line => {
           if (line.type == 'paragraph') {
-            return <p>{line.content}</p>
+            return <p key={line.content}>{line.content}</p>
           } else if (line.type == 'link') {
-            return <p><a href="">{line.content}</a></p>
+            return <p key={line.content}><a href="">{line.content}</a></p>
           } else if (line.type == 'hash') {
-            return <a href="">{line.content}{' '}</a>
+            return <a key={line.content} href="">{line.content}{' '}</a>
           }
         })}
       </div>
@@ -65,18 +87,34 @@ export function Post({ author, publishedAt, content }) {
         <strong>Deixe seu feedback</strong>
 
         <textarea
+          name="comment"
           placeholder='Deixe um comentário'
+          value={newCommentText}
+          onChange={handleNewCommentChange}
+          onInvalid={handleNewCommentInvalid}
+          required
         />
 
         <footer>
-          <button type='submit'>Publicar</button>
+          <button
+            type='submit'
+            disabled={isNewCommentEmpty}
+          >
+            Publicar
+          </button>
         </footer>
 
       </form>
 
       <div className={styles.commentList}>
         {comments.map(comment => {
-          return <Comment />
+          return (
+            <Comment
+              key={comment}
+              content={comment}
+              onDeleteComment={deleteComment}
+            />
+          );
         })}
       </div>
     </article>
